e397743edc3007ca043aceae282742691e26cd45
radman1007 <radman.cod110@gmail.com>
2024-07-04 16:58:41 +0330

    add url

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .forms import LoginForm
 
 app_name = 'social'
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('logout/', views.log_out, name="logout"),
 ]
