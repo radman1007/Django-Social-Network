0c551a3ffe18758576d5bba7420e28eabe8eb4c5
radman1007 <radman.cod110@gmail.com>
2024-07-04 16:41:14 +0330

    debug

--- social/views.py
+++ social/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 
 def log_out(request):
     logout(request)
