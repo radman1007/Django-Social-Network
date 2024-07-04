f5dfd81ff737c2636ad6db806a886858e31e49a9
radman1007 <radman.cod110@gmail.com>
2024-07-04 12:37:17 +0330

    debug

--- social/models.py
+++ social/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    photo = models.ImageField(upload_to="", blank=True, null=True)
+    photo = models.ImageField(upload_to="profile/", blank=True, null=True)
     job = models.CharField(max_length=250, null=True, blank=True)
     phone = models.CharField(max_length=11, null=True, blank=True)
