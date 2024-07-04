f5452522c920d76f7b33d9d1e5599b76ee76192b
radman1007 <radman.cod110@gmail.com>
2024-07-04 13:05:43 +0330

    debug

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social.apps.SocialConfig'
+    'social.apps.SocialConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- social/urls.py
+++ social/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = 'social'
+
 urlpatterns = [
 ]
