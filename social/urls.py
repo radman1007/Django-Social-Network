b095c7866bdee093dd057d465a0bc01a301f095c
radman1007 <radman.cod110@gmail.com>
2024-07-04 17:06:59 +0330

    debug

--- social/urls.py
+++ social/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from . import views 
 from .forms import LoginForm
+from django.contrib.auth import views as auth_views
 
 app_name = 'social'
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
     path('logout/', views.log_out, name="logout"),
 ]
