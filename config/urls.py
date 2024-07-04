8cc50986a5bb8e32878295856a34808435517d3f
radman1007 <radman.cod110@gmail.com>
2024-07-04 12:52:26 +0330

    debug for media

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('social.urls', namespace='social'))
-]
+] + static(settings.BASE_DIR, document_root=settings.MEDIA_ROOT)
